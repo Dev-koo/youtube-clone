fc1d5a7d418fb6092ca3744d16e41a5982309faf
Dev-koo <gyu523@naver.com>
2023-05-08 20:16:35 +0900

    feature:Implement client side routing with react-router-dom v6

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/", // main page
+    element: <App />,
+    errorElement: <h1>404 Not found page</h1>,
+  },
+  {
+    path: "/videos/:keyword", //search page
+    element: <div>Vidoes</div>,
+  },
+  {
+    path: "/watch/:videoId", // watching video page
+    element: <div>Watch</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
